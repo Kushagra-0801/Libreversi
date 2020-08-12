5f0405978eea22865a3dae0b9df0220cc9a40032
Kushagra Gupta <kushgpt08@gmail.com>
2020-08-12 19:36:59 +0530

    Add getters for position and fix from impl

--- src/position.rs
+++ src/position.rs
@@ -8,13 +8,35 @@ pub struct Position {
     pub(crate) idx: u8,
 }
 
+impl Position {
+    pub fn row(&self) -> usize {
+        ((self.idx >> 3) & 0b111) as usize
+    }
+
+    pub fn col(&self) -> usize {
+        (self.idx & 0b111) as usize
+    }
+}
+
 impl From<(u8, u8)> for Position {
     fn from(p: (u8, u8)) -> Self {
         if p.0 > 7 || p.1 > 7 {
             panic!("Index out of bounds")
         }
         Self {
-            idx: p.0 << 3 + p.1,
+            idx: (p.0 << 3) + p.1,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_getters() {
+        let pos: Position = (3, 6).into();
+        assert_eq!(pos.col(), 6);
+        assert_eq!(pos.row(), 3);
     }
 }
